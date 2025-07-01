2504081dfb018cd77d4a6783b5c5897c1800b7d7
aadeshdevganiya <devganiyaaadesh2023@gmail.com>
2025-07-01 16:09:35 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import Navbar from "./component/Navbar";
 import HomePage from "./component/HomePage";
 import Footer from "./component/Footer";
+import Preloader from "./component/Preloader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) return <Preloader />;
+
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <HomePage />
       <Footer />
     </>
   );
