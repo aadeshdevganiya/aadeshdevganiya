bbed6a8c7e21b9c18529f28707a0141ea47377d8
aadeshdevganiya <devganiyaaadesh2023@gmail.com>
2025-07-02 09:56:26 +0530

    preloader

--- src/component/Preloader.jsx
+++ src/component/Preloader.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./Preloader.css";
 
 const Preloader = () => {
-  return <></>;
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-[100vh]">
+        <div className="scan">
+          <div className="fingerprint"></div>
+          <h3>Scanning...</h3>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Preloader;
